e2e89a0cd41f78c9bd40ad504cfda8fd8da7d01e
aydensbadger <aydensbadger@gmail.com>
2023-02-13 13:17:06 -0500

    filled more sudo code

--- paint.py
+++ paint.py
@@ -7,10 +7,8 @@ import pygame as pg
 
 pg.init()
 
-WIDTH, HEIGHT = 800, 895 # x, y
+WIDTH, HEIGHT = 800, 900 # x, y
 FPS = 60 # Frames Per Second
-COLOR = (240, 240, 240) # RGB
-BLACK = (0, 0, 0) # RGB
 
 images_path = fr'{Path(__file__).parents[0]}\images'
 # image_path = fr'{images_path}\drawing.png'
@@ -22,27 +20,29 @@ class Paint:
     def __init__(self, surface, word):
         self.surface = surface
         self.word = word
-        self.colors = button.Button(surface, pg.image.load(f'{images_path}\colors.png'), 2.87)
-        self.all_buttons = {'red': 823,
-                'blue': 0,
-                'green': 0,
-                'cyan': 0,
-                'pink': 0,
-                'yellow': 0,
-                'white': 0,
-                'black': 0,
-                'erase': 0,
-                'finish': 0}
+        self.colors = button.Button(surface, pg.image.load(f'{images_path}\colors2.png'), 2.9)
+        self.all_buttons = {'red': 19,
+                'blue': 97,
+                'green': 175,
+                'cyan': 254,
+                'magenta': 332,
+                'yellow': 410,
+                'white': 488,
+                'black': 567,
+                'erase': 645,
+                'finish': 723
+                }
 
     def play(self):
-        for color, y in self.all_buttons.items():
-            if button.Button(self.surface, pg.image.load(f'{images_path}\\blank.png'), 2.87).click(23, y):
+        self.colors.draw(0, 800)
+        for color, x in self.all_buttons.items():
+            if button.Button(self.surface, pg.image.load(f'{images_path}\\blank.png'), 2.87).click(x, 824):
                 self.color = self.change_color(color)
 
     def change_color(self, color):
         if color == 'erase':
             # Recursion
-            main(self.word)
+            main(self.word, started=True)
         elif color == 'finish':
             self.finish()
 
@@ -53,7 +53,7 @@ class Paint:
         quit()
 
 
-def main(word=None):
+def main(word=None, started=False):
     if not word:
         word = choice(mod.words)
 
@@ -68,15 +68,14 @@ def main(word=None):
     paint = Paint(surface, word)
 
     start = button.Button(surface, pg.image.load(f'{images_path}\start.png'), 4)
-    started = False
     
     font1 = pg.font.Font(font_path, 160, bold=True)
-    text = font1.render(word.upper(), True, BLACK)
+    text = font1.render(word.upper(), True, 'black')
     textRect = text.get_rect()
     textRect.center = (500, 400)
 
     font2 = pg.font.Font(font_path, 150, bold=True)
-    text2 = font2.render('Your word is: ', True, BLACK)
+    text2 = font2.render('Your word is: ', True, 'black')
     textRect2 = text2.get_rect()
     textRect2.center = (520, 200)
 
@@ -90,7 +89,7 @@ def main(word=None):
         clock.tick(FPS)
 
         # Resetting screen every tick
-        surface.fill(COLOR)
+        surface.fill('white')
 
         if started or start.click(30, 550):
             started = True
@@ -100,7 +99,6 @@ def main(word=None):
             surface.blit(text2, textRect2)
 
         pg.display.update()
-
 
 
 if __name__ == "__main__":
